4d2431344f62b1eebf87a6394b10e0972eb18680
Sultomaj <sultomaj@gmail.com>
2025-12-09 12:28:49 +0300

    Remove unused index.css import
    Removed import statement for index.css as it is no longer needed.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// We deleted index.css, so we don't import it anymore!
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
